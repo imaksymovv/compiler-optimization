3c8ef83654aa4e10c8a76ab8e3078038fe69fa30
Ivan Maksymov <Ivan.Vasiliy.Maksymov@gmail.com>
2023-06-27 16:47:53 +0200

    add files to the program add average value calculation

--- chrono_test/chrono_test.cpp
+++ chrono_test/chrono_test.cpp
@@ -1,22 +1,46 @@
-﻿#include <iostream>
+﻿#include <chrono>
 #include <cmath>
-#include <chrono>
+#include <fstream>
+#include <iostream>
 
 int main() {
-	int numbers[1000];
+  int numbers[1000];
+  std::fstream info(
+      "info.txt", std::ios_base::in | std::ios_base::out | std::ios_base::app);
+  int duration = 0;
+  if (info) {
+    auto start = std::chrono::high_resolution_clock::now();
+    for (size_t i = 0; i < 1000; i++) {
+      numbers[i] = i;
+    }
+    for (size_t i = 0; i < 1000; i++) {
+      numbers[i] = pow(numbers[i], 2);
+    }
+    auto end = std::chrono::high_resolution_clock::now();
 
-  auto start = std::chrono::high_resolution_clock::now();
-  for (size_t i = 0; i < 1000; i++) {
-       numbers[i] = i;
+    auto dur = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start)
+                   .count();
+    info << dur << std::endl;
+
+    info.seekg(0);  
+
+    int number;
+    int sum = 0;
+    int count = 0;
+
+    while (info >> number) {
+      sum += number;
+      count++;
+    }
+
+    double average = 0;
+    if (count > 0) {
+      average = static_cast<double>(sum) / count;
+    }
+
+    std::cout << "Average: " << average << std::endl;
+  } else {
+    std::cout << "file is not opened" << std::endl;
   }
-  for (size_t i = 0; i < 1000; i++) {
-    numbers[i] = pow(numbers[i], 2);
-  }
-  auto end = std::chrono::high_resolution_clock::now();
-
-  auto dur = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start)
-                 .count();
-
-  std::cout << dur;
-	return 0;
+  return 0;
 }
