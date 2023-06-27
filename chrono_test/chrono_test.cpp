051de769107be8dc13f44ee87b5ebaecc6c8a457
Ivan Maksymov <Ivan.Vasiliy.Maksymov@gmail.com>
2023-06-27 18:29:56 +0200

    remove one cycle rename file

--- chrono_test/chrono_test.cpp
+++ chrono_test/chrono_test.cpp
@@ -6,15 +6,12 @@
 int main() {
   int numbers[1000];
   std::fstream info(
-      "info.txt", std::ios_base::in | std::ios_base::out | std::ios_base::app);
+      "SomeFile.txt", std::ios_base::in | std::ios_base::out | std::ios_base::app);
   int duration = 0;
   if (info) {
     auto start = std::chrono::high_resolution_clock::now();
     for (size_t i = 0; i < 1000; i++) {
-      numbers[i] = i;
-    }
-    for (size_t i = 0; i < 1000; i++) {
-      numbers[i] = pow(numbers[i], 2);
+      numbers[i] = pow(i, 2);
     }
     auto end = std::chrono::high_resolution_clock::now();
 
